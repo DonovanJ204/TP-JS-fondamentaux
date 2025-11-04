6e3b11ec61dd371463fbd64c39a4fba174d125e9
DonovanJ204 <donovan.jeannot.stcharles@gmail.com>
2025-11-04 07:44:41 +0000

    feat: Maîtrise des variables et conditions

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log("Laboratoire prêt !");
+
+const nom = "Votre Nom";
+let age = 17;
+
+const motDePasseAttendu = "secret123";
+let motDePasseUtilisateur = "secret123"; // Changez cette valeur pour tester
+
+let longueur = motDePasseUtilisateur.length
+
+if (motDePasseUtilisateur === motDePasseAttendu) {
+    console.log("Accès autorisé. Bienvenue !");
+} else if (longueur < 8) {
+    console.log("Accès refusé. Votre mot de passe est trop court (8 caractères min).");
+} else {
+    console.log("Accès refusé. Mot de passe incorrect.");
+}
+
