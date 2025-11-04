b0b69dde53b234c84246fdaa514115e58eb8a8df
DonovanJ204 <donovan.jeannot.stcharles@gmail.com>
2025-11-04 08:01:11 +0000

    feat: Maîtrise des Objets JS

--- script.js
+++ script.js
@@ -16,3 +16,11 @@ if (motDePasseUtilisateur === motDePasseAttendu) {
     console.log("Accès refusé. Mot de passe incorrect.");
 }
 
+const moi = {
+    prenom: "Donovan",
+    nom: "Jeannot",
+    age: 18,
+    competences: ["HTML", "CSS"]
+};
+
+console.log(`Je m'appelle ${moi.prenom} ${moi.nom}`)
